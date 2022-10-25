e8e81da9077870f4dd034fe95e227184e6aac09a
Wendril Avila <57228649+TheWendril@users.noreply.github.com>
2022-10-25 14:57:54 -0300

    add id e imagem de capa no modelo de post

--- src/Models/PostModel.rs
+++ src/Models/PostModel.rs
@@ -4,9 +4,11 @@ use rocket::serde::{Serialize, Deserialize};
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 pub struct PostModel{
+    pub id: i8,
     pub title: String,
+    pub image: String,
     pub content: String,
-    pub author: String,
+    pub author: i8,
     pub tag: String,
     pub likes: i32
 }
