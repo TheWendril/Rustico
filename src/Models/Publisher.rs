fec0dea98cb664dd65b1b4a0ac08f361eb78b91f
Wendril Avila <57228649+TheWendril@users.noreply.github.com>
2022-10-25 15:00:10 -0300

    add senha e foto de perfil no modelo de Publisher

--- src/Models/Publisher.rs
+++ src/Models/Publisher.rs
@@ -5,7 +5,10 @@ use rocket::serde::{Serialize, Deserialize};
 #[serde(crate = "rocket::serde")]
 pub struct Publisher{
     name: String,
+    access_level: i8,
+    password: String,
+    picture: String, 
     bio: String,
     education: String, 
-    age: i16
+    age: i8
 }
