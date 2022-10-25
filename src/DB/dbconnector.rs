b42b3a17300135de8d0bd8107c64da86008775f1
Wendril Avila <wendril2001avila@gmail.com>
2022-10-25 19:20:17 -0300

    migrando de mongodb para sqlite

--- src/DB/dbconnector.rs
+++ src/DB/dbconnector.rs
@@ -1,2 +1,18 @@
-use mongodb::{bson::doc, Client};
+use rocket::http::private::Connection;
+use sqlite;
 
+
+struct DbConnector{
+    
+    conn: sqlite::Connection
+ 
+}
+
+impl DbConnector{    
+
+    fn new() -> Self{
+           
+        Self { conn: sqlite::open("~/rusticodb").unwrap() }
+    }
+
+}
