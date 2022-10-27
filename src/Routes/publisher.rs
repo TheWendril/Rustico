4de9d635dc2f5a0d0653d335af4156966c26ec72
Wendril Avila <57228649+TheWendril@users.noreply.github.com>
2022-10-27 14:01:58 -0300

    add metodo get_publisher_by_id

--- src/Routes/publisher.rs
+++ src/Routes/publisher.rs
@@ -1,7 +1,7 @@
 // Que Deus me perdoe por esse código...
 // Escrito por Wendril Avila
 
-use super::super::Models::Publisher::Publisher;
+use super::super::Models::Publisher::{Publisher, PublisherBuilder};
 use super::super::DB::dbconnector;
 use rocket::{serde::{json::Json}, http::Status};
 
@@ -12,20 +12,30 @@ use rocket::{serde::{json::Json}, http::Status};
 pub fn get_publisher_by_id(publisher_id: i8) -> Json<Publisher> {
 
     let dbcon: dbconnector::DbConnector = dbconnector::DbConnector::new();
-    let mut query_string: String = String::from(" SELECT * FROM posts WHERE title = '");   
-    query_string.push_str(post_title);
-    query_string.push_str("'");
-    
+    let mut query_string: String = String::from(" SELECT * FROM posts WHERE id = ");   
+    query_string.push_str(publisher_id.as_str());
+
+
     let mut query_result = dbcon.conn.prepare(&query_string.as_str()).unwrap();
 
     let mapped_rows = query_result.query_map([], |row| {
-        Ok(create_post_model(row.get(0)?, row.get(1)?, row.get(2)?, row.get(3)?, row.get(4)?, row.get(5)?))
-    }).unwrap();
+        Ok( PublisherBuilder::new()
+                             .id(row.get(0)?)
+                             .name(row.get(1)?)
+                             .email(row.get(2)?)
+                             .access_level(row.get(3)?)
+                             .password(row.get(4)?)
+                             .picture(row.get(5)?)
+                             .bio(row.get(6)?)
+                             .education(row.get(7)?)
+                             .age(row.get(8)?)
+                             .build(row.get(9)?);
+        }).unwrap();
 
-    let new_post_model: PostModel = mapped_rows.into_iter()
+    let new_publisher: Publisher = mapped_rows.into_iter()
                                                .next()
                                                .unwrap()
                                                .ok()
                                                .unwrap();
-    return Json(new_post_model)
+    return Json(new_publisher)
 }
